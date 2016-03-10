d722676283ee71a719eeeb3615e320a53bd11a41
Fernando Rodriguez <frr@agbo.biz>
2016-03-10 11:13:47 -0800

    created classes from entities

---
+++ Note.swift
@@ -0,0 +1,17 @@
+//
+//  Note.swift
+//  CoolNotes
+//
+//  Created by Fernando Rodríguez Romero on 10/03/16.
+//  Copyright © 2016 udacity.com. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Note: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
